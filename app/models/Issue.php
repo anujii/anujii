330907c59e2c111259b25ba77eec7795a9d3fa4b
nomenes <maxklukin@gmail.com>
2013-06-21 19:23:05 +0400

    issue hidden fields

--- app/models/Issue.php
+++ app/models/Issue.php
@@ -2,19 +2,21 @@
 
 /**
  * @property integer $id
+ * @property string $subject
+ * @property string $description
  * @property mixed $created_at
  * @property mixed $updated_at
- * @property string $subject
  * @property mixed $project
  * @property mixed $author
  * @property mixed $assigned_to
  * @property mixed $started_at
  * @property mixed $ended_at
- * @property string $description
  */
 class Issue extends Eloquent {
 
 	protected $table = 'issues';
 
 	protected $fillable = array('subject', 'description');//for now
+
+	protected $hidden = array('created_at', 'updated_at');
 }
